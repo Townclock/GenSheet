070d5170fe5d01e53dbbd414c277b460a428ef7f
Buzz Tilford <tilford20@gmail.com>
2021-01-17 18:13:57 -0800

    removed as structure, rules only, propagation working

--- main.js
+++ main.js
@@ -2,56 +2,42 @@
 let ALN = 1000000  // arbitrarily large number
 
 
-function Sum(total, num) {return total + num;}
-
-let possibleChords = [
-  {type: "I", weight:1},
-  {type: "ii", weight:1},
-  {type: "V", weight:1},
-  ]
-        // this array will contain all possible chords 
-        // that can be picked, chord objects
-let chordPossibilities = [];
-for (i = 0; i < possibleChords.length; i++)
-  chordPossibilities.push(true);
-let startingTotalWeight = 0;
-for (i = 0; i < possibleChords.length; i++)
-  startingTotalWeight += possibleChords[i].weight;
-
 let chordRules = [
-  {lead:"I", follow:"I", mod: 4},
-  {lead:"I", follow:"ii", mod: 0},
-  {lead:"ii", follow:"V", mod: 0},
-  {lead:"V", follow:"I", mod: 0},
-  {lead:"V", follow:"ii", mod: -2}
+  {lead:"I", follow:"I", mod: 4, weight: 1},
+  {lead:"I", follow:"ii", mod: 0, weight: 1},
+  {lead:"ii", follow:"V", mod: 0, weight: 1},
+  {lead:"V", follow:"I", mod: 0, weight: 1},
+  {lead:"V", follow:"ii", mod: -2, weight: 1}
 ]
 
+let startingTotalWeight = 0;
+for (i = 0; i < chordRules.length; i++)
+  startingTotalWeight += chordRules[i].weight;
 
-let model = {
-  // array of sections
-  // section of specific type are required to be similar
-  // each section will contain 32 points which may be "holds"
-  wave: [
-    {type:'A', content:[], weights:[]},
-    {type:'A', content:[], weights:[]},
-    {type:'B', content:[], weights:[]},
-    {type:'A', content:[], weights:[]}
-  ],
 
-  stack: [], //stack of tuples in ref code
-  stackSize: 0,
 
-}
+let model;
 
 function Init(){
+
+  model= {
+    // array of sections
+    // section of specific type are required to be similar
+    wave: [
+      {type:'A', content:[], weights:[]},
+      {type:'A', content:[], weights:[]},
+      {type:'B', content:[], weights:[]},
+      {type:'A', content:[], weights:[]}
+    ]
+  }
+  
   model.wave.forEach(function(section){
     for (i=0; i < 32; i++){
-      section.content.push(possibleChords.slice(0, chordPossibilities.length));
+      section.content.push(chordRules.slice(0, chordRules.length));
       section.weights.push(startingTotalWeight);
     }
     
   })
-  console.log(model.wave)
 }
 
 
@@ -64,8 +50,6 @@ function Observe()
   let loc = locations.slice(0, 128).indexOf(locations.sort(function(a,b){return a-b})[0]);
   let section = Math.floor(loc / 32);
   let locInSection = loc % 32;
-  console.log(locations)
-  console.log(section)
 
   // generate a random value and select one of the possible chords
   let pick = Math.random()*model.wave[section].weights[locInSection];
@@ -73,7 +57,6 @@ function Observe()
     if (pick < model.wave[section].content[locInSection][i].weight) {
       let winner = model.wave[section].content[locInSection][i];
       model.wave[section].content[locInSection] = [winner];
-      console.log(winner);
       model.wave[section].weights[locInSection] = ALN;
     }
     else 
@@ -88,8 +71,8 @@ function CheckComplete (){
     locations = locations.concat(section.weights)
   })
   if (locations.sort(function(a,b){return a-b})[0] < ALN)
-    return true
-  return false
+    return false
+  return true
   
 }
 
@@ -97,7 +80,7 @@ function Propagate (section, loc){
   let previous, next, adjactents;
   if (loc === 0){
     if (section === 0)
-      previous = [4, 31]
+      previous = [3, 31]
     else
       previous = [section-1, 31]
   }
@@ -105,41 +88,125 @@ function Propagate (section, loc){
     previous = [section, loc-1]
 
   if (loc === 31){
-    if (section === 4)
-      next = [0, 1]
+    if (section === 3)
+      next = [0, 0]
     else
-      next = [section+1, 1]
+      next = [section+1, 0]
   }
   else next = [section, loc+1]
+
   let sectionType = model.wave[section].type;
   adjacents = model.wave.filter(function(e){return e.type === sectionType});
 
 
-//check next
-//check previous
-console.log(adjacents)
-adjacents.forEach(function(adjacent){
-  adjacent = model.wave.indexOf(adjacent)
-  model.wave[adjacent].weights[loc] = model.wave[section].weights[loc]
-  model.wave[adjacent].content[loc] = model.wave[section].content[loc]
-})
+  // assume that there are no changes that need to be propagate
+  let propagateNext = false;
+  let propagatePrevious = false;
+
+  // get all the rules for next
+  let nextRules = model.wave[next[0]].content[next[1]];
+  // get rules for origin
+  let focusRules = model.wave[section].content[loc]
+
+  let rulesToRemove = [];
+  // check all rules of next and remove them, propogate if changed
+  nextRules.forEach(function(followingRule, index){
+    // assume no rules are valid
+    let valid = false
+    focusRules.forEach(function(leadingRule){
+      if (leadingRule.follow == followingRule.lead){
+        valid = true;
+      }
+    })
+    if (!valid)
+      rulesToRemove.push(index);
+  })
+  // remove the rules
+  let removalOffset = 0;
+  rulesToRemove.forEach(function(rule){
+    model.wave[next[0]].weights[next[1]] -= nextRules[rule-removalOffset].weight;
+    nextRules.splice(rule-removalOffset, 1)
+    removalOffset +=1;
+  });
+  if (rulesToRemove.length > 0)
+        propagateNext = true;
+
+  // get all the rules for previous
+  let previousRules = model.wave[previous[0]].content[previous[1]];
+
+  rulesToRemove = [];
+  // check all rules of next and remove them, propogate if changed
+  previousRules.forEach(function(leadingRule, index){
+    // assume no rules are valid
+    let valid = false
+    focusRules.forEach(function(followingRule){
+      if (leadingRule.follow == followingRule.lead){
+        valid = true;
+      }
+    })
+    if (!valid)
+      rulesToRemove.push(index);
+  })
+  // remove the rules
+  removalOffset = 0;
+  rulesToRemove.forEach(function(rule){
+    model.wave[previous[0]].weights[previous[1]] -= previousRules[rule-removalOffset].weight;
+    previousRules.splice(rule-removalOffset, 1)
+    removalOffset +=1;
+  });
+  if (rulesToRemove.length > 0)
+        propagatePrevious = true;
 
 
-// constrain all of these according to section, loc
-// if any of them loose possibilities, call propogate on them 
-// recursive propgation calls across sections are unnessesary
+  if (propagateNext) Propagate(next[0], next[1]);
+  if (propagatePrevious) Propagate(previous[0], previous[1]);
+
+
+  // create consistency across sections
+  adjacents.forEach(function(adjacent){
+    adjacent = model.wave.indexOf(adjacent)
+    let weight = model.wave[adjacent].weights[loc] 
+    model.wave[adjacent].weights[loc] = model.wave[section].weights[loc]
+    model.wave[adjacent].content[loc] = model.wave[section].content[loc]
+    
+    if (weight !== model.wave[adjacent].weights[loc])
+      Propagate(adjacent, loc)
+  })
+
+}
+ 
+
+function Print(){
+  let output = ""
+  model.wave.forEach(function(section){
+    section.content.forEach(function(content) {
+      output += content[0].follow + "(" + content[0].mod + ")" + "\t"
+    
+    })
+    output += "<br>"
+  })
+
+
+
+  document.getElementById("chords").innerHTML = output;
 }
 
 
 
-// make sure to update weights as propogation constrains chords options
-
-
-
-
-
-
-
+let br = 500;
+Init()
+while (!CheckComplete()){
+  Observe()
+  console.log("done?")
+  br--;
+  if (br < 0){
+    Init();
+    console.log("break loop")
+    br = 500;
+  }
+};
+console.log("DONE")
+Print()
 
 
 
